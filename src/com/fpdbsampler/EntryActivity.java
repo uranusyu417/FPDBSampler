e46d991cff4c680a4c204a4df4d0fcc00ec04d79
uranusyu417 <uranusyu417@gmail.com>
2014-04-10 22:11:27 +0800

    entry page partial design

---
+++ src/com/fpdbsampler/EntryActivity.java
@@ -0,0 +1,14 @@
+package com.fpdbsampler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EntryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
